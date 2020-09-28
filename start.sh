760f651b2dca1f06fdbd2485ca529d77846649a5
WALFRAN ROCHA VIEIRA CARVALHO DE ARAÚJO <walfran.carvalho@unifesp.br>
2020-09-28 16:05:37 -0300

    correção alter table

--- start.sh
+++ start.sh
@@ -2,5 +2,5 @@ docker-compose exec heliosapp python manage.py syncdb
 docker-compose exec heliosapp python manage.py migrate
 docker-compose exec -u root heliosapp python manage.py compilemessages 
 docker-compose exec heliosbd psql -U helios -d helios -a -c "ALTER TABLE auth_user ALTER COLUMN username TYPE varchar(50);"
-docker-compose exec heliosbd psql -U helios -d helios -a -c "ALTER TABLE auth_user ALTER COLUMN firts_name TYPE varchar(100);"
+docker-compose exec heliosbd psql -U helios -d helios -a -c "ALTER TABLE auth_user ALTER COLUMN first_name TYPE varchar(100);"
 docker-compose exec heliosbd psql -U helios -d helios -a -c "ALTER TABLE auth_user ALTER COLUMN last_name TYPE varchar(100);"
